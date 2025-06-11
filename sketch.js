fa51dce2e61ce35339c19f703b80aa2177ca87de
Marcos Hernández <126048022+marcosnigp1@users.noreply.github.com>
2025-06-11 14:37:21 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+let w = window.innerWidth;
+let h = window.innerHeight;  
+
 function setup() {
-  createCanvas(400, 400);
+  canvas = createCanvas(w, h);
 }
 
 function draw() {
-  background(225);
+  background(0);
+
+  fill(255,0,0);
+
+  //Abdullah
+  push()
+  translate(w*0.35,h*0.25)
+  //rect(0,0,w*0.10,h*0.20);
+  pop()
+  //
+
+
+  //Marcos
+
+
+  //
+}
+
+function windowResized() {
+  // assigns new values for width and height variables
+  w = window.innerWidth;
+  h = window.innerHeight;  
+  canvas.resize(w,h);
 }
